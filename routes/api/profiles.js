9bbbda1f9e9fd95addefdeaf97cd171f3a59ac42
Eric Thande <ericthande@Erics-MacBook-Pro.local>
2022-12-11 14:54:44 +0200

    Delete a profile & User

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -143,6 +143,30 @@ router.get("/user/:user_id", async (res,req)=> {
 
 })
 
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private 
+router.delete("/",auth, async (res,req)=> {
+    try {
+        // @todo -remove users profile
+
+        //remove profile
+         await Profile.findOneAndRemove({user:req.user.id})
+         // remove user
+         await User.findOneAndRemove({_id:req.user.id})
+
+res.json({msg: "User Deleted"})
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server Error")
+        
+    }
+
+})
+
+
+
 
 
 module.exports = router
