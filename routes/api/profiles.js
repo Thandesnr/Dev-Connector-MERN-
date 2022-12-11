81fc0c7ed10f718924d55d37a5c1a3c5141c14b4
Eric Thande <ericthande@Erics-MacBook-Pro.local>
2022-12-11 16:56:07 +0200

    Deleting the profile experience

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -222,6 +222,30 @@ router.put("/experience",[auth], [
 
 })
 
+// @route    DELETE api/profile/experience/:exp_ id
+// @desc     Delete experience from profile
+// @access   Private
+
+router.delete("/experience/:exp_id",auth, async (req,res) =>{
+    try {
+        const profile = await findOne({user: req.user.id})
+        
+        // Getting the correct experience to remove ie Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+        
+        profile.experience.splice(removeIndex,1)
+
+        await profile.save()
+res.json(profile)
+        
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({msg:"Server Error" })
+        
+    }
+})
+
+
 
 
 
