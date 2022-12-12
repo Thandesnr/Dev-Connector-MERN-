d84b4329cc696c15bbda432b68cf7614d5a61525
Eric Thande <ericthande@Erics-MBP.Home>
2022-12-12 12:03:56 +0200

    GET Github repos for the profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const auth =  require("../../middleware/auth")
 const Profile = require("../../models/Profile")
 const {check,validationResult } = require("express-validator")
+const request = require("request")
+const config = require("config")
 
 
 const User = require("../../models/User")
@@ -324,6 +326,37 @@ res.json(profile)
         res.status(500).json({msg:"Server Error" })
         
     }
+})
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+
+router.get("/github/:username", async (req,res) =>{
+try {
+    const options ={
+        uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&
+        sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=
+        ${config.get("githubSecret")}`,
+        method:"GET",
+        headers:{"user-agent":"node.js"}
+    }
+
+    request(options,(error,response, body) =>{
+        if(error) console.error(error)
+
+        if(response.statusCode !==200){
+            return res.status(404).json({msg:"Github profile not found"})
+
+            res.json(JSON.parse(body))
+        }
+    })
+    
+} catch (error) {
+    console.error(err.message)
+    res.status(500).send("Server Error")
+    
+}
 })
 
 
